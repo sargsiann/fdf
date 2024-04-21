dbf5b73db3e9e1bf82b01e754216119db77c1b6c
Davit Sargsyan <dasargsy@c2r5s6.42yerevan.am>
2024-04-21 15:43:10 +0400

    finished with introduction with minilibix

--- main.c
+++ main.c
@@ -1,159 +1,99 @@
 #include "mlx.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-#define WINDOW_WIDTH 500
-#define WINDOW_HEIGHT 500
-
-#define MLX_ERROR 1
-
-#define RED_PIXEL 0xFF0000
-#define GREEN_PIXEL 0x00FF00
-
-typedef struct s_img{
-	void *mlx;
-	char *address;
-	int	bpp;
-	int line_size;
-	int endian;
-}	t_img;
-
-typedef struct s_data {
-	void	*mlx;
-	void	*win;
-	t_img	*img;
-}				t_data;
-
-
-typedef struct s_rect
+typedef struct s_img
 {
-    int	x;
-    int	y;
-    int width;
-    int height;
-    int color;
-}	t_rect;
+    void    *mlx;
+    void    *win;
+    void    *img;
+    char    *addr;
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+} t_img;
 
-int	color_encoder(int red, int green, int blue)
+typedef struct s_pixel
 {
-	return (red << 16 | green << 8 | blue);
-}
-
-void	my_pixel_put(t_img *img, int x, int y, int color)
-{
-    char    *pixel;
-
-    pixel = img->address + (y * img->line_size + x * (img->bpp / 8));
-    *(int *)pixel = color;
-}
-
-void	render_background(t_data *data, int color)
-{
-    int	i;
-    int	j;
-
-    if (data->win == NULL)
-        return ;
-    i = 0;
-    while (i < WINDOW_HEIGHT)
-    {
-        j = 0;
-        while (j < WINDOW_WIDTH)
-            my_pixel_put(data->img, j++, i, color);
-        ++i;
-    }
-}
-
-
-int render_rect(t_data *data, t_rect rect)
-{
-    int	i;
-    int j;
-
-    if (data->win == NULL)
-        return (1);
-    i = rect.y;
-    while (i < rect.y + rect.height)
-    {
-        j = rect.x;
-        while (j < rect.x + rect.width)
-            my_pixel_put(data->img, j++, i, rect.color);
-        ++i;
-    }
-    return (0);
-}
-
-int render(void *param)
-{
-	t_data *data = (t_data *)param;
-	render_background(data, 0xFFFFFF);
-	render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100,
-            100, 100, GREEN_PIXEL});
-    render_rect(data, (t_rect){0, 0, 100, 100, RED_PIXEL});
-	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
-    return (0);
-}
-
-int expose_handler(void *param)
-{
-	printf("Hello\n");
-	return 0;
-}
-
-int	mouse_handler(int mouse, int x, int y, void *param)
-{
-	t_data *data = (t_data *)param;
-	if (mouse == 1)
-		mlx_pixel_put(data->mlx, data->win, x, y, 0x0000000FF);
-	else if (mouse == 2)
-		mlx_pixel_put(data->mlx, data->win, x, y, 0x000000000);
-	else if (mouse == 3)
-		printf("Middle click\n");
-	return (0);
-}
-
+    int x;
+    int y;
+} t_pixel;
 
 int key_handler(int keycode, void *param)
 {
-	t_data *data = (t_data *)param;
-
-	if (keycode == 53)
+	if (keycode == 53) // 53 is the keycode for ESC key
 	{
-		system("leaks a.out");
 		exit(0);
 	}
-	printf("keycode: %d\n", keycode);
-	return (0);
+	return 0;
+}
+
+int rgb_to_int(unsigned char red, unsigned char green, unsigned char blue)
+{
+    return (red << 16) | (green << 8) | blue;
+}
+
+void put_pixel_to_image(t_img *img, t_pixel pixel, int color)
+{
+    char *dst = img->addr + (pixel.y * img->line_length + pixel.x * (img->bits_per_pixel / 8));
+	if (img->endian == 0)
+	{
+		*(unsigned char *)dst = color;
+		*(unsigned char *)(dst + 1) = color >> 8;
+		*(unsigned char *)(dst + 2) = color >> 16;
+		*(unsigned char *)(dst + 3) = color >> 24;
+	}
+	else if (img->endian == 1)
+	{
+		*(unsigned char *)dst = color >> 16;
+		*(unsigned char *)(dst + 1) = color >> 8;
+		*(unsigned char *)(dst + 2) = color;
+		*(unsigned char *)(dst + 3) = color >> 24;
+	}
+}
+
+void fill_image_with_color(t_img *img, int width, int height, int color)
+{
+    int x, y;
+    t_pixel pixel;
+
+    for (y = 0; y < height; y++)
+    {
+        for (x = 0; x < width; x++)
+        {
+            pixel.x = x;
+            pixel.y = y;
+            put_pixel_to_image(img, pixel, rgb_to_int(x,y,x + y));
+        }
+    }
+}
+
+int loop_handler(void *param)
+{
+    t_img *img = (t_img *)param;
+    int     win_width = 800;
+    int     win_height = 600;
+    int     fill_color = 0x00FF0000; // Red color
+
+    fill_image_with_color(img, win_width, win_height, fill_color);
+    mlx_put_image_to_window(img->mlx, img->win, img->img, 0, 0); // Display the updated image
+
+    return 0;
 }
 
 int main()
 {
-	void	*mlx;
-	void	*win;
-	struct s_data data;
+    t_img   *img;
+    int     win_width = 800;
+    int     win_height = 600;
+    
+    img = (t_img *)malloc(sizeof(t_img));
+    img->mlx = mlx_init();
+    img->win = mlx_new_window(img->mlx, win_width, win_height, "Fill Window with Color");
+    img->img = mlx_new_image(img->mlx, win_width, win_height);
+    img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	mlx_key_hook(img->win, key_handler, (void *)0); // Set the exit handler
+    mlx_loop_hook(img->mlx, loop_handler, (void *)img); // Set the loop handler
+    mlx_loop(img->mlx);
 
-	mlx = mlx_init();
-	if (mlx == NULL)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	win = mlx_new_window(mlx, 500, 500, "Hello World!");
-	if (win == NULL)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	data.mlx = mlx;
-	data.win = win;
-	data.img = mlx_new_image(mlx, 500, 500);
-	data.img->address = mlx_get_data_addr(data.img,&(data.img->bpp),&(data.img->line_size),&(data.img->endian));
-	mlx_key_hook(win, key_handler, &data);
-	mlx_mouse_hook(win, mouse_handler, &data);
-	mlx_expose_hook(win, expose_handler, &data);
-	mlx_loop_hook(mlx, render, &data);
-	mlx_loop(mlx);
-	system("leaks a.out");
-	return (0);
+    return 0;
 }
