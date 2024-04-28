5e1e7b5ab3be1d9a971ead08f5bfccf55930793e
sargsyandavid7 <sargsyand648@gmail.com>
2024-04-28 16:39:56 +0400

    starting from 0 point

--- srcs/draw/rgb_to_int.c
+++
@@ -1,6 +0,0 @@
-#include "../../fdf.h"
-
-int	rgb_to_int(int r, int g, int b)
-{
-	return (r << 16 | g << 8 | b);
-}
--- srcs/hooks/loop_hooks.c
+++
@@ -1,7 +0,0 @@
-#include "../../fdf.h"
-
-int	loop_hooks(t_data *data)
-{
-	mlx_put_image_to_window(data->mlx, data->win, data->img->img, 0, 0);
-	return (0);
-}
